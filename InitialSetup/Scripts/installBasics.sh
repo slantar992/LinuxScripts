bd42e1cf76cdd5c8a331a2e70f7a0659fe056c06
Slantar <albertouma92@gmail.com>
2020-12-28 13:57:49 +0100

    add more configuration

--- InitialSetup/Scripts/installBasics.sh
+++ InitialSetup/Scripts/installBasics.sh
@@ -2,7 +2,13 @@
 sudo apt-get -y install snap snapd
 sudo apt-get -y install xclip
 sudo apt-get -y install tilix
+sudo apt-get -y install python3-nautilus
 sudo apt-get -y install git-lfs
+sudo apt-get -y install mmv
+#install remmina
+sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
+sudo apt update
+sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
 #install keeweb
 wget -O keeweb.deb https://github.com/keeweb/keeweb/releases/download/v1.15.7/KeeWeb-1.15.7.linux.x64.deb
 sudo dpkg -i keeweb.deb
